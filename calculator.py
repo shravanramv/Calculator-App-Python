609cd3c686009753af0ebde3042c67e3a80e86f8
Shravan Ram V <shravanv.ram@gmail.com>
2024-08-21 12:15:57 +0800

    Changes to app styling

--- calculator.py
+++ calculator.py
@@ -1,8 +1,20 @@
 import flet as ft
 
+# Define custom button class
+class MyButton(ft.ElevatedButton):
+    def __init__(self, text, is_operation=False, **kwargs):
+        super().__init__(**kwargs)  # Pass other keyword arguments like 'expand'
+        self.text = text
+        if is_operation:
+            self.bgcolor = ft.colors.ORANGE_300
+            self.color = ft.colors.BLACK
+        else:
+            self.bgcolor = ft.colors.BLACK
+            self.color = ft.colors.WHITE
+
 def main(page: ft.Page):
     page.title = "Calculator App"
-    page.window.resizable = True  # Allow the window to be resizable
+    page.window.resizable = True
     page.window.width = 400
     page.window.height = 600
 
@@ -10,54 +22,54 @@ def main(page: ft.Page):
 
     page.add(
         ft.Container(
-            expand=True,  # Make the container fill the available space
+            expand=True,
             padding=20,
             content=ft.Column(
-                expand=True,  # Make the column fill the available space
+                expand=True,
                 controls=[
                     ft.Row(controls=[result], alignment="end"),
                     ft.Row(
                         expand=True,
                         controls=[
-                            ft.ElevatedButton(text="AC", expand=True),
-                            ft.ElevatedButton(text="+/-", expand=True),
-                            ft.ElevatedButton(text="%", expand=True),
-                            ft.ElevatedButton(text="/", expand=True),
+                            MyButton(text="AC", expand=True),
+                            MyButton(text="+/-", expand=True),
+                            MyButton(text="%", expand=True),
+                            MyButton(text="/", is_operation=True, expand=True),
                         ]
                     ),
                     ft.Row(
                         expand=True,
                         controls=[
-                            ft.ElevatedButton(text="7", expand=True),
-                            ft.ElevatedButton(text="8", expand=True),
-                            ft.ElevatedButton(text="9", expand=True),
-                            ft.ElevatedButton(text="*", expand=True),
+                            MyButton(text="7", expand=True),
+                            MyButton(text="8", expand=True),
+                            MyButton(text="9", expand=True),
+                            MyButton(text="*", is_operation=True, expand=True),
                         ]
                     ),
                     ft.Row(
                         expand=True,
                         controls=[
-                            ft.ElevatedButton(text="4", expand=True),
-                            ft.ElevatedButton(text="5", expand=True),
-                            ft.ElevatedButton(text="6", expand=True),
-                            ft.ElevatedButton(text="-", expand=True),
+                            MyButton(text="4", expand=True),
+                            MyButton(text="5", expand=True),
+                            MyButton(text="6", expand=True),
+                            MyButton(text="-", is_operation=True, expand=True),
                         ]
                     ),
                     ft.Row(
                         expand=True,
                         controls=[
-                            ft.ElevatedButton(text="1", expand=True),
-                            ft.ElevatedButton(text="2", expand=True),
-                            ft.ElevatedButton(text="3", expand=True),
-                            ft.ElevatedButton(text="+", expand=True),
+                            MyButton(text="1", expand=True),
+                            MyButton(text="2", expand=True),
+                            MyButton(text="3", expand=True),
+                            MyButton(text="+", is_operation=True, expand=True),
                         ]
                     ),
                     ft.Row(
                         expand=True,
                         controls=[
-                            ft.ElevatedButton(text="0", expand=True),
-                            ft.ElevatedButton(text=".", expand=True),
-                            ft.ElevatedButton(text="=", expand=True),
+                            MyButton(text="0", expand=True),
+                            MyButton(text=".", expand=True),
+                            MyButton(text="=", is_operation=True, expand=True),
                         ]
                     ),
                 ]
